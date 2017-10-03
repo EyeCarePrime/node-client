329a4a1b9a0a227b77fad56d97c54693c70f6c05
akannalathws3 <akannalath@websystem3.com>
2017-10-03 11:06:02 -0700

    connection error handling

--- index.js
+++ index.js
@@ -74,6 +74,7 @@ var new_client = function(sdk_key, config) {
         }
         
         config.logger.error("[LaunchDarkly]", error);
+        client.emit('error') // AK - altered from launch darkly repository to handle errors
       }
       else if (!init_complete) {
         init_complete = true;        
